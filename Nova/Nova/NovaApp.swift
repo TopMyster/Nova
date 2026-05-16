93260ea32afbaaeeb70499b3672cc24837e532d7
TopMyster <topmyster@outlook.com>
2026-05-16 18:33:47 -0500

    feat: Adds browser functions, searchbar, and a bit of styling

---
+++ Nova/Nova/NovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovaApp.swift
+//  Nova
+//
+//  Created by Toope Oladunjoye on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct NovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nova/Nova/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  Nova
+//
+//  Created by Toope Oladunjoye on 5/16/26.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
