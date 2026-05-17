b8ae583fd2ee5ddadc102629426bfc5bc6f2f96e
TopMyster <topmyster@outlook.com>
2026-05-16 23:02:23 -0500

    feat: Tabs now work but don't have animations

---
+++ Nova/BrowserTab.swift
@@ -0,0 +1,13 @@
+//
+//  BrowserTab.swift
+//  Nova
+//
+//  Created by Toope Oladunjoye on 5/16/26.
+//
+
+import Foundation
+
+struct BrowserTab: Identifiable, Equatable {
+    let id = UUID()
+    var url: String
+}
