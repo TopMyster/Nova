a6b6b2fa592f9bea669503f5e6ac3e832410f7b1
TopMyster <topmyster@outlook.com>
2026-05-16 22:43:46 -0500

    featL Adds a non functioning tab system

---
+++ Nova/Nova/TabsView.swift
@@ -0,0 +1,18 @@
+//
+//  TabsView.swift
+//  Nova
+//
+//  Created by Toope Oladunjoye on 5/16/26.
+//
+
+import SwiftUI
+
+struct TabsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabsView()
+}
